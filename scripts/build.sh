a3ce83e4f936dd3f0eaecf60269b6f5a627e9a3a
Shane Poppleton <shane@bjja.com.au>
2024-06-06 22:09:26 +1000

    Fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+DEV_DIR=./deploy/dev
+PROD_DIR=./deploy/prod
+
+mkdir -p "$DEV_DIR/priv/static"
+mkdir -p "$PROD_DIR/priv/static"
 
 echo "Building project..."
 gleam run -m lustre/dev build app
@@ -6,22 +11,22 @@ gleam run -m lustre/dev build app
 if [ $? -eq 0 ]; then
 	echo "Build successful. Copying files..."
 
-	cp -r priv ./deploy/dev/priv
-	cp index.html ./deploy/dev/
+	cp -r ./priv "$DEV_DIR/"
+	cp index.html "$DEV_DIR/"
 	echo "Done."
 
 	echo "Building for production..."
-	cp index.html ./deploy/prod/
+	cp index.html "$PROD_DIR/"
 
 	echo "Performing minification..."
-	find ./deploy/dev/priv -name '*.mjs' | while read -r file; do
-		outfile=$(echo "$file" | sed 's/dev/prod/' | sed 's/\.mjs/\.min\.mjs/')
+	find "$DEV_DIR/priv" -name '*.mjs' | while read -r file; do
+		outfile=$(echo "$file" | sed 's|deploy/dev/priv|deploy/prod/priv|' | sed 's/\.mjs/\.min\.mjs/')
 		mkdir -p $(dirname "$outfile")
 		terser "$file" --compress --mangle --output "$outfile"
 	done
 
-	find ./deploy/dev/priv -name '*.css' | while read -r file; do
-		outfile=$(echo "$file" | sed 's/dev/prod/' | sed 's/\.css/\.min\.css/')
+	find "$DEV_DIR/priv" -name '*.css' | while read -r file; do
+		outfile=$(echo "$file" | sed 's|deploy/dev/priv|deploy/prod/priv|' | sed 's/\.css/\.min\.css/')
 		mkdir -p $(dirname "$outfile")
 		csso "$file" --output "$outfile"
 	done
